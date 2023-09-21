06aeba7d4569660ca51272332f602b74a598c218
white <ntukhashvili@cu.edu.ge>
2023-09-21 15:12:51 +0400

    Initial commit

---
+++ src/main/java/org/example/BasicCommands.java
@@ -0,0 +1,99 @@
+package org.example;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+/* გამოსადეგი shortcut-ები
+   psvm - ქმნის main მეთოდს
+   sout - პრინტ მეთოდი კონსოლში დასაბეჭდად
+ */
+
+public class BasicCommands {
+    public static void main(String[] args) throws InterruptedException {
+        //ვუთითებთ ქრომის დრაივერის ლოკაციას
+        System.setProperty("webdriver.chrome.driver", "C:/Users/white/Documents/browser-drivers/chromedriver.exe");
+
+        //ვქმნით chrome ბრაუზერის ინსტანს, ავტომატურად იხსნება ცარიელი ფანჯარა
+        WebDriver driver = new ChromeDriver();
+        //ვამატებთ ლოდინს იმსითვის რომ დავჰელნდოთ შესაძლო exception throws InterruptedException უნდა დავუმატოთ main methods
+        Thread.sleep(1000);
+        //ერთდროულად შესაძლებელია რამდენიმე სხვადასხვა ბრაუზერის ინსტანსის შექმნა თუ დაინსტალირებულია firefox ავტომატურად გაეშვება
+        //WebDriver firefoxDriver = new FirefoxDriver();
+
+        //ბრაუზერი გაეშვება დიდი ზომით მთლიანად ფანჯარაში
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+
+        //ბრაუზერის ფანჯარა დაპატარავდება
+        driver.manage().window().minimize();
+
+        Thread.sleep(1000);
+        //შესაძლებელია ბრაუზერის ფანჯრისთვის კონკრეტული ზომების მითითება
+        //setSize მეთოდი იღებს პარამატერად Dimmension კლასის ობიექტს სადაც უნდა გავდეთ ორი პარამეტრი x/y ზომები
+        driver.manage().window().setSize(new Dimension(1280,720));
+
+        Thread.sleep(1000);
+        //ბრაუზერით კონკრეტულ მისამართზე გადასვლა
+        driver.get("https://www.google.com");
+
+        Thread.sleep(1000);
+        //driver.get და navigate().to() ერთი და იგივე ბრძანებებია და არანაიი განსხვავება არაა მათ შორის
+        driver.navigate().to("https://www.facebook.com");
+
+        Thread.sleep(1000);
+        //ბრაუზერის back ღილაკი
+        driver.navigate().back();
+
+        Thread.sleep(1000);
+        //ბრაუზერის forward ღილაკი
+        driver.navigate().forward();
+
+        Thread.sleep(1000);
+        //ფანჯრის განახლება/დარეფრეშება
+        driver.navigate().refresh();
+
+        Thread.sleep(1000);
+        //ახალი ტაბის გახსნა
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+        //მეორე ტაბში გადავდივართ ვიკიპედიის გვერდზე
+        driver.get("https://www.wikipedia.org");
+
+        //შეგვიძლია ავიღოთ ტაბის title (იმ ტაბის რომელიცაა ამ წუთას დაფოსირებული)
+        String title = driver.getTitle();
+        //ვიღებთ URL იმ გვერდის რომელზეც ვიმყოფებით
+        String currentURL = driver.getCurrentUrl();
+
+        System.out.println(title);
+        System.out.println(currentURL);
+
+        //ვეძებთ ელემენტს id-ით
+        WebElement searchInput =  driver.findElement(By.id("searchInput"));
+        //ვწერთ სასურველ ტექსტს საძიებო ველსი
+        searchInput.sendKeys("Georgia");
+
+        //ვეძებთ ელემემენტს xpath-ით
+        WebElement searchIcon = driver.findElement(By.xpath("//i[@data-jsl10n = 'search-input-button']"));
+        searchIcon.click();
+
+        Thread.sleep(1000);
+        //ვიღებთ ტექსტს კონრეტული ელემენტიდან ვეძებთ კლასის სახელით
+        String searchResultTitle =  driver.findElement(By.className("mw-page-title-main")).getText();
+        System.out.println(searchResultTitle);
+
+        Thread.sleep(1000);
+        //ბრაუზერის ტაბის დახურვა
+        driver.close();
+
+        //ბრაუზერის ფანჯრის(ყველა ტაბის) დახურვა
+        driver.quit();
+        Thread.sleep(1000);
+
+
+    }
+}
---
+++ src/main/java/org/example/FormFill.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+//მაგალითი ფორმის შევსების
+
+public class FormFill {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().setSize(new Dimension(1280,720));
+
+        driver.get("https://qavalidation.com/demo-form/");
+
+        driver.findElement(By.id("g4072-fullname")).sendKeys("Username");
+        driver.findElement(By.id("g4072-email")).sendKeys("Password");
+        driver.findElement(By.id("g4072-phonenumber")).sendKeys("57716651");
+
+        //ვხსნით dropdown menu-ს
+        driver.findElement(By.id("g4072-gender-button")).click();
+        WebElement dropdownMenu = driver.findElement(By.xpath("//ul[@id='g4072-gender-menu']"));
+
+        //dropdown menu-ის ელემენტებს ვინახავთ ლისტში ვიყენებთ findElements რადგან ვიცით რომ რამდენიმე option-ი გვაქ
+        List<WebElement> dropDownOptions = dropdownMenu.findElements(By.className("ui-menu-item"));
+
+        //ვაკლიკებთ პირველივე options dropdownmenu-ში
+        dropDownOptions.get(0).click();
+
+        //ვეძებთ radiobutton-ების მშობელელ ელემენტს შემდეგ კი მის შვილ ელემენტებს
+        List<WebElement> radioButtons = driver
+        .findElement(By.className("grunion-radio-options"))
+        .findElements(By.name("g4072-yearsofexperience"));
+
+        //ვაკლიკებთ ყველა radio ბათონს სათითაოდ მიმდევრობით
+        for (WebElement el:radioButtons
+             ) {
+                Thread.sleep(1000);
+                el.click();
+        }
+
+        //xpath ლოკატორით ვაკლიკებთ მეორე ელემენტს
+        driver.findElement(By.xpath("//div[@class='grunion-checkbox-multiple-options']//label[1]")).click();
+
+        //id ლოკატორით ვეძებთ text fields და ვწერთ ტექსტს
+        driver.findElement(By.id("contact-form-comment-g4072-otherdetails")).sendKeys("SOME TEXT");
+
+        //className ლოკატორით ვეძებთ submit ღილაკს და ვაკლიკეთ
+        driver.findElement(By.className("wp-block-button__link")).click();
+
+        driver.quit();
+
+    }
+}
