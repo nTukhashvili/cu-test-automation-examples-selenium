3772ad84a53ae461aca1aab8dd8f91dcfd189eb7
white <ntukhashvili@cu.edu.ge>
2023-10-02 16:18:54 +0400

    removed path to taget html file

--- src/main/java/Solutions/Task_2.java
+++ src/main/java/Solutions/Task_2.java
@@ -15,11 +15,11 @@ public class Task_2 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("C:\\Users\\white\\Desktop\\task2.html");
+        driver.get("Path to task2.html");
         driver.findElement(By.xpath("//*[@id='fName']")).sendKeys("Nika");
         driver.findElement(By.cssSelector("input[data-selenium='lName']")).sendKeys("Tukhashvili");
         driver.findElement(By.name("date")).sendKeys("10291998");
-        driver.findElement(By.xpath("//*[@id='gender']")).click();
+        driver.findElement(By.xpath("//*[@id='gender']/div[@class='form-check-inline'][2]")).click();
         WebElement item1 =  driver.findElement(By.xpath("//*[@id='sortable']/li[1]"));
         WebElement item4 =  driver.findElement(By.xpath("//*[@id='sortable']/li[4]"));
 
@@ -35,7 +35,7 @@ public class Task_2 {
                 .perform();
         Thread.sleep(2000);
 
-        // ველოდებით მანამ სანამ საძიებო ელემენტში მეორე არგუმენტად გადაწოდებული სტრინგი
+        // ველოდებით მანამ სანამ target ელემენტში მეორე არგუმენტად გადაწოდებული ტექსტი არ გამოჩნდება
         wait.until(ExpectedConditions.textToBe(By.id("confirmBox"),"Confirmed"));
 
         driver.findElement(By.xpath("//button[@data-btn='submitBtn']")).click();
